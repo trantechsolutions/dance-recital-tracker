36dae66cf97c2110af5ef964c49e950f27b55e66
Jonathan V Tran <me@jonathanvutran.com>
2026-03-04 21:03:39 -0600

    updated browser routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { AppProvider } from './context/AppContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AppProvider>
         <App />
       </AppProvider>
